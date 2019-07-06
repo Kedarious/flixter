0a5a8e4f2df0d74b9ad5930b79624ef212258190
kedarious <darious_petway@yahoo.com>
2019-07-06 21:29:08 +0000

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'flixter-kedarious-petway.herokuapp' }
+ end 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
