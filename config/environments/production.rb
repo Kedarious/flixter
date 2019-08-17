e2fc31e11c1fed80aea13bed96f0ba02f0438878
kedarious <darious_petway@yahoo.com>
2019-08-17 16:05:24 +0000

    fixing heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'flixter-kedarious-petway.herokuapp' }
- end 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -93,4 +92,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
